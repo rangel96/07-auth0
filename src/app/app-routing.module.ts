bb5feb8b08c0a1fa2b0be61de3e94ea67ab9a5d4
Eric Rangel Carmona <dreamereric28@gmail.com>
2021-01-12 15:34:38 -0600

    AuthApp: Autenticación con auth0 y uso de guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { HomeComponent } from './components/home/home.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+import { PriceComponent } from './components/price/price.component';
+import { AuthGuard } from './services/auth.guard';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'prices', component: PriceComponent },
+  { path: 'protected', component: ProtectedComponent, canActivate: [AuthGuard] },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from '@auth0/auth0-angular';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+import { PriceComponent } from './components/price/price.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtectedComponent,
+    PriceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    AuthModule.forRoot({
+      domain: 'ericran90.us.auth0.com',
+      clientId: 'hXJY2i2Ptkr5pIw1ShJKLCtJSbz1kM6t'
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
